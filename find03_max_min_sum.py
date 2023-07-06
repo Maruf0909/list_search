fe76236d4ce9be9129f36c34591b372d58252d7d
Mac <mac@MacBook-Air-Mac.local>
2023-07-06 17:59:16 +0500

    find3

--- find03_max_min_sum.py
+++ find03_max_min_sum.py
@@ -5,4 +5,17 @@ def find_max_min_sum(data):
         data: list of numbers
     returns: sum of the maximum and minimum numbers in the list
     """
-    return 0
+    max=data[0]
+    min=data[0]
+    i=1
+    while i<len(data):
+        if max<data[i]:
+            max=data[i]
+        
+        if min>data[i]:
+            min=data[i]
+        i+=1
+        sum=max+min
+    return sum
+numbers=[5,7,3,4,8]
+print(find_max_min_sum(numbers))
