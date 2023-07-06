5f8960b36280c6e489471f7a84b00f0654a37b48
Mac <mac@MacBook-Air-Mac.local>
2023-07-06 19:39:45 +0500

    find6

--- find06_max_odd.py
+++ find06_max_odd.py
@@ -5,4 +5,18 @@ def find_max_odd(data):
         data: list of numbers
     returns: maximum odd number in the list
     """
-    return 0
+    odd=[]
+    i=0
+    while i<len(data):
+        if data[i]%2==1:
+            odd.append(data[i])
+        i+=1
+    max=odd[0]
+    i=0
+    while i<len(odd):
+        if max<odd[i]:
+            max=odd[i]
+        i+=1
+    return max
+numbers=[5,6,4,2,8,4,3]
+print(find_max_odd(numbers))
