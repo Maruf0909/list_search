e8332e6ec52a2170d3641730377f2266613b3a7d
Mac <mac@MacBook-Air-Mac.local>
2023-07-06 20:29:32 +0500

    find8

--- find08_min_count.py
+++ find08_min_count.py
@@ -5,4 +5,13 @@ def find_min_count(data):
         data: list of numbers
     returns: count of minimum numbers in the list
     """
-    return 0
+    min_count=data[0]
+    i=1
+    while i<len(data):
+        if min_count>data[i]:
+            min_count=data[i]
+        i+=1
+
+    return data.count(min_count)
+numbers=[6,5,3,3,3,8,6,5,9,3,8,5]
+print(find_min_count(numbers))
