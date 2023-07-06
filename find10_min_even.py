73c06023f8325953eb1decffcd1fd66f1b2cb843
Mac <mac@MacBook-Air-Mac.local>
2023-07-06 20:51:57 +0500

    find10

--- find10_min_even.py
+++ find10_min_even.py
@@ -5,5 +5,22 @@ def find_min_even(data):
         data: list of numbers
     returns: minimum even number in the list
     """
-    return 0
+    evens=[]
+    i=0
+    while i<len(data):
+        if data[i]%2==0:
+            evens.append(data[i])
+        i+=1
+    
+    min=evens[0]
+    i=0
+    while i<len(evens):
+        if min>evens[i]:
+            min=evens[i]
+        i+=1    
+
+
+    return min
+numbers=[4,7,6,9,21,6,8]
+print(find_min_even(numbers))
 
