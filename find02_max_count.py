3288c639605c7a71a94fce5cad25d2fb0a32a295
Mac <mac@MacBook-Air-Mac.local>
2023-07-06 17:35:18 +0500

    find2

--- find02_max_count.py
+++ find02_max_count.py
@@ -5,4 +5,13 @@ def find_max_count(data):
         data: list of numbers
     returns: count of maximum numbers in the list
     """
-    return 0
+    max=data[0]
+    i=1
+    while i<len(data):
+        if max<data[i]:
+            max=data[i]
+        i+=1
+    return data.count(max)
+numbers=[1,8,2,8,3,8,4,8,5]
+print(find_max_count(numbers))
+    
