4eda15e5e419b83c49e8a733f6a59f4f725a7440
Mac <mac@MacBook-Air-Mac.local>
2023-07-06 21:04:38 +0500

    find11

--- find11_min_odd.py
+++ find11_min_odd.py
@@ -5,5 +5,22 @@ def find_min_odd(data):
         data: list of numbers
     returns: minimum odd number in the list
     """
-    return 0
+    odd=[]
+    i=0
+    while i<len(data):
+        if data[i]%2==1:
+            odd.append(data[i])
+        i+=1
+
+    min=odd[0]
+    i=0
+    while i<len(odd):
+        if min>odd[i]:
+            min=odd[i]
+        i+=1
+    return min
+numbers=[7,4,8,3,2,9,5]
+print(find_min_odd(numbers))
+
+    
 
