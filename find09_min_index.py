7f69d00684fa9c48157f1016dd0f4139ddc3e11d
Mac <mac@MacBook-Air-Mac.local>
2023-07-06 20:38:19 +0500

    find9

--- find09_min_index.py
+++ find09_min_index.py
@@ -5,5 +5,13 @@ def find_min_index(data):
         data: list of numbers
     returns: index of minimum number in the list
     """
-    return 0
+    min_index=data[0]
+    i=1
+    while i<len(data):
+        if min_index>data[i]:
+            min_index=data[i]
+        i+=1
+    return data.index(min_index)
+numbers=[8,7,3,6,1,2,3,4,5,6,7]
+print(find_min_index(numbers))
 
