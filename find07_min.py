47d462503d4d4c0ced1f4a3ca028130bc01538a5
Mac <mac@MacBook-Air-Mac.local>
2023-07-06 20:09:28 +0500

    find7

--- find07_min.py
+++ find07_min.py
@@ -5,5 +5,15 @@ def find_min(data):
         data: list of numbers
     returns: minimum number in the list
     """
+    min=data[0]
+    i=1
+    while i<len(data):
+        if min>data[i]:
+            min=data[i]
+        i+=1
+    return min
+numbers=[3,1,5,2,9,6]
+print(find_min(numbers))
 
-    return 0
+
+    
